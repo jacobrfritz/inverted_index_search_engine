87165339157818201f749c33524e131f8431af79
GiggleBytes <throwaway699023742987423@gmail.com>
2026-05-20 23:18:21 -0500

    show file names for top match when you search

--- src/inverted_index_search_engine/tui.py
+++ src/inverted_index_search_engine/tui.py
@@ -1,6 +1,6 @@
 from textual import work
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, RichLog, Input
+from textual.widgets import Header, Footer, RichLog, Input, ListView, ListItem, Label
 from rich.table import Table
 from inverted_index_search_engine.search_engine import SearchEngine
 
@@ -9,11 +9,10 @@ textual tui that triggers the search engine service to a begin a search or switc
 """
 
 
-
 class Tui(App):
-    def __init__(self, search_engine:SearchEngine):
+    def __init__(self, search_engine: SearchEngine):
         super().__init__()
-        self.search_engine = search_engine 
+        self.search_engine = search_engine
 
     BINDINGS = [("ctrl+d", "toggle_dark", "Toggle dark mode")]
 
@@ -21,15 +20,22 @@ class Tui(App):
         """Create child widgets for the app."""
         yield Header()
         yield Input()
-        yield RichLog()
+        yield ListView(id="file_results")
         yield Footer()
 
     @work(thread=True, exclusive=True)
     def on_input_changed(self, event: Input.Changed) -> None:
         """Event handler called when a button is pressed."""
-        text_log = self.query_one(RichLog)
-        text_log.clear()
+        
+        results_list = self.query_one("#file_results", ListView)
+        self.call_from_thread(results_list.clear)
+        
         current_text = event.value.strip()
         if current_text:
-            ranked_matches = self.search_engine.get_ranked_matches(current_text)
-            text_log.write('\n'.join([f'{word}: Files: {item['files']}\nPositions: {item['positions']}' for word,item in ranked_matches.items()]))
+            ranked_matches = self.search_engine.get_ranked_matches(current_text, 1)
+            for word, item in ranked_matches.items():
+                files = set(item['files'])
+                self.call_from_thread(results_list.append, ListItem(Label(word)))
+                for file in sorted(files, key=lambda f: f.path.name):
+                    item_widget = ListItem(Label(file.path.name))
+                    self.call_from_thread(results_list.append, item_widget)
