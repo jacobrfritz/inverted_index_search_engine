fc2dbc508c2894df239e5d2f653a7637e70c0f32
GiggleBytes <throwaway699023742987423@gmail.com>
2026-05-21 22:48:00 -0500

    file name suggestions

--- src/inverted_index_search_engine/tui.py
+++ src/inverted_index_search_engine/tui.py
@@ -1,13 +1,13 @@
 from textual import work
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, RichLog, Input, ListView, ListItem, Label
+from textual.widgets import Header, Footer, Input, ListView, ListItem, Label
+from textual.suggester import SuggestFromList
 from rich.table import Table
 from inverted_index_search_engine.search_engine import SearchEngine
 
 """
 textual tui that triggers the search engine service to a begin a search or switch to a new one
 """
-
 
 class Tui(App):
     def __init__(self, search_engine: SearchEngine):
@@ -19,23 +19,45 @@ class Tui(App):
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
-        yield Input()
+        yield Input(
+            placeholder="Type a search query...",
+            id="search_input"  # Added an ID so we can easily query it
+        )
         yield ListView(id="file_results")
         yield Footer()
 
     @work(thread=True, exclusive=True)
     def on_input_changed(self, event: Input.Changed) -> None:
-        """Event handler called when a button is pressed."""
+        """Event handler called when the input text changes."""
         
         results_list = self.query_one("#file_results", ListView)
+        search_input = self.query_one("#search_input", Input)
+        
+        # Clear previous results safely from the thread
         self.call_from_thread(results_list.clear)
         
         current_text = event.value.strip()
         if current_text:
             ranked_matches = self.search_engine.get_ranked_matches(current_text, 1)
+            
+            new_suggestions = list(ranked_matches.keys())
+            
+            if new_suggestions:
+                self.call_from_thread(
+                    lambda: setattr(
+                        search_input, 
+                        "suggester", 
+                        SuggestFromList(new_suggestions, case_sensitive=False)
+                    )
+                )
+            else:
+                self.call_from_thread(lambda: setattr(search_input, "suggester", None))
+
             for word, item in ranked_matches.items():
                 files = set(item['files'])
-                self.call_from_thread(results_list.append, ListItem(Label(word)))
+                
                 for file in sorted(files, key=lambda f: f.path.name):
                     item_widget = ListItem(Label(file.path.name))
                     self.call_from_thread(results_list.append, item_widget)
+        else:
+            self.call_from_thread(lambda: setattr(search_input, "suggester", None))
